3eef07bbf6901950451465bcfde3b92a5379d1df
Ramesh N <111989928+Raam043@users.noreply.github.com>
2022-11-17 21:35:18 +0530

    Create security.grp.allowcation.tf

---
+++ Terraform/security.grp.allowcation.tf
@@ -0,0 +1,21 @@
+resource "aws_security_group" "allow_port80" {
+  name        = "allow_port80"
+  description = "Allow Inbound Traffic on Port 80"
+
+  ingress {
+    description      = "Port 80 from Everywhere"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
