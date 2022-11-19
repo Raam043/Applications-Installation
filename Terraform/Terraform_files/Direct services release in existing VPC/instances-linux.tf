860738c555ce67dcb1b958009b2a6b3c8f8270fa
Ramesh N <111989928+Raam043@users.noreply.github.com>
2022-11-19 13:03:52 +0530

    Update instances-linux.tf

--- Terraform/Terraform_files/Direct services release in existing VPC/instances-linux.tf
+++ Terraform/Terraform_files/Direct services release in existing VPC/instances-linux.tf
@@ -16,7 +16,7 @@ yum update -y
 yum install -y httpd.x86_64
 systemctl start httpd.service
 systemctl enable httpd.service
-echo "<Body background="https://github.com/Raam043/Pipeline/blob/master/Sai.jpg?raw=true"><body><div align="center"><h1><strong><p style="color:yellow"> Ramesh Biradar </p></strong></h1><h2><p style="color:greenyellow">Welcome to Saikrishna's html World</p></h2><p style="color:green">Call Mr. Ramesh N for enquiries <a href="tel:9902163099"style="color:yellow"><strong>9902163099</strong></a>.</p></style></head><a href="https://github.com/Raam043/Pipeline/blob/master/Sai3.jpeg?raw=true"> <img src="https://github.com/Raam043/Pipeline/blob/master/Sai3.jpeg?raw=true" alt="Image" style="width:300px height:400px" /> </a><a href="https://github.com/Raam043/Pipeline/blob/master/Sai5.jpeg?raw=true"> <img src="https://github.com/Raam043/Pipeline/blob/master/Sai5.jpeg?raw=true" alt="Image" style="width:300px height:400px" /> </a><a href="https://github.com/Raam043/Pipeline/blob/master/Sai6.jpeg?raw=true"> <img src="https://github.com/Raam043/Pipeline/blob/master/Sai6.jpeg?raw=true" alt="Image" style="width:300px height:400px" /> </a>" > /var/www/html/index.html
+echo "<Body background="https://github.com/Raam043/Pipeline/blob/master/Sai.jpg?raw=true"><body><div align="center"><h1><strong><p style="color:yellow"> Ramesh </p></strong></h1><h2><p style="color:greenyellow">Welcome to Saikrishna's html World</p></h2><p style="color:green">Call Mr. Ramesh N for enquiries <a href="tel:9902163099"style="color:yellow"><strong>9902163099</strong></a>.</p></style></head>" | sudo tee /var/www/html/index.html
     EOF
     tags = {
     Name = "Ramesh-Linux"
